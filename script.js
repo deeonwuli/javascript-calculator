5f85607a03a1ba3418947fa0a43f15554de59936
Chukwudumebi Onwuli <onwulidumebi@gmail.com>
2021-04-18 12:58:39 +0100

    Update script.js to handle errors

--- script.js
+++ script.js
@@ -11,11 +11,16 @@ function calculate(event) {
     const clicked_value = event.target.value
     if (clicked_value === '=') {
       if (display.value !== '') {
-        display.value = eval(display.value);
+        if (display.value = NaN) {
+            display.value = NaN
+            console.log(NaN)
+        }
+        display.value = eval(display.value)
+        console.log(display.value)
       }
     } else if (clicked_value === 'AC') {
-      display.value = '';
+      display.value = ''
     } else {
-      display.value += clicked_value;
+      display.value += clicked_value
     }
   }
